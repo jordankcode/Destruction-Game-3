fd87ec5b8be26f5e3cdc854dde30caec570de638
jordankcode <monkefin@gmail.com>
2026-03-06 14:45:46 +1030

    Movement

--- Assets/PlayerInputHandler.cs
+++ Assets/PlayerInputHandler.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Input Action Asset")]
+    [SerializeField] private InputActionAsset playerControls;
+
+    [Header("Action Map Name Reference")]
+    [SerializeField] private string actionMapName = "Player";
+
+    [Header("Action Map Name References")]
+    [SerializeField] private string movement = "Movement";
+    [SerializeField] private string rotation = "Rotation";
+    [SerializeField] private string jump = "Jump";
+    [SerializeField] private string sprint = "Sprint";
+
+    private InputAction movementAction;
+    private InputAction rotationAction;
+    private InputAction jumpAction;
+    private InputAction sprintAction;
+
+    public Vector2 MovementInput { get; private set; }
+    public Vector2 RotationInput { get; private set; }
+    public bool JumpTriggered { get; private set; }
+    public bool SprintTriggered { get; private set; }
+
+    private void Awake()
     {
-        
+        InputActionMap mapReference = playerControls.FindActionMap(actionMapName);
+
+        movementAction = mapReference.FindAction(movement);
+        rotationAction = mapReference.FindAction(rotation);
+        jumpAction = mapReference.FindAction(jump);
+        sprintAction = mapReference.FindAction(sprint);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SubscribeActionValuesToInputEvents()
     {
-        
+        movementAction.performed += inputInfo => MovementInput = inputInfo.ReadValue<Vector2>();
+        movementAction.canceled += inputInfo => MovementInput = Vector2.zero;
+
+        rotationAction.performed += inputInfo => RotationInput = inputInfo.ReadValue<Vector2>();
+        rotationAction.performed += inputInfo => RotationInput = inputInfo.ReadValue<Vector2>();
     }
 }
