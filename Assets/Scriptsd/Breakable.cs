e5c8592de97022ba3dc2a84c38660f9faa42574c
jordankcode <monkefin@gmail.com>
2026-03-11 10:08:29 +1030

    workiong breaking object

--- Assets/Scriptsd/Breakable.cs
+++ Assets/Scriptsd/Breakable.cs
@@ -1,16 +1,33 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Breakable : MonoBehaviour
 {
+
+    public Rigidbody rb;
+
+    public bool isBroken;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb.isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isBroken==true)
+        {
+            rb.isKinematic = false;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Breaker")
+        {
+            isBroken = true;
+        }
     }
 }
