714ace1dc92cf5263f96c4dd8ec5fe2c29c9dbe9
jordankcode <monkefin@gmail.com>
2026-03-10 11:16:22 +1030

    FIRST PERSON CONTROLLER
    first cehcpoint i guess

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -39,11 +39,65 @@ public class FirstPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+        HandlRotation();
     }
 
     private Vector3 CalculateWorldDirection()
     {
-        Vector3 inputDirection = new Vector3
+        Vector3 inputDirection = new Vector3(playerInputHandler.MovementInput.x, 0, playerInputHandler.MovementInput.y);
+        Vector3 worldDirection = transform.TransformDirection(inputDirection);
+        return worldDirection.normalized;
+    }
+
+    private void HandleJumping()
+    {
+        if (characterController.isGrounded)
+
+        {
+            currentMovement.y = -0.5f;
+
+            if (playerInputHandler.JumpTriggered)
+            {
+                currentMovement.y = jumpForce;
+            }
+
+        }
+        else
+        {
+            currentMovement.y += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 worldDirection = CalculateWorldDirection();
+        currentMovement.x = worldDirection.x * CurrentSpeed;
+        currentMovement.z = worldDirection.z * CurrentSpeed;
+
+        HandleJumping();
+        characterController.Move(currentMovement * Time.deltaTime);
+
+    }
+
+    private void ApplyHorizontalRotation(float rotationAmount)
+    {
+        transform.Rotate(0, rotationAmount, 0);
+    }
+
+    private void ApplyVerticalRotation(float rotationAmount)
+    { 
+        verticalRotation = Mathf.Clamp(verticalRotation - rotationAmount, -upDownLookRange, upDownLookRange);
+        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+
+    private void HandlRotation()
+    {
+       float mouseXRotation = playerInputHandler.RotationInput.x * mouseSensitivity;
+       float mouseYRotation = playerInputHandler.RotationInput.y* mouseSensitivity;
+
+        ApplyHorizontalRotation(mouseXRotation);
+        ApplyVerticalRotation(mouseYRotation);
     }
 }
